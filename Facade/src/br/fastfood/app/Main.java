54cf7c0aae26a0c306c30e8d9c860c4e849c7383
Glória <gmpianheri@gmail.com>
2025-04-27 11:35:57 -0300

    cria a main

--- Facade/src/br/fastfood/app/Main.java
+++ Facade/src/br/fastfood/app/Main.java
@@ -3,25 +3,24 @@
 
 package br.fastfood.app;
 import java.util.Scanner;
-//import br.fastfood.facade.ComboFacade;
+import br.fastfood.facade.ComboFacade;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("=== Menu de Combos ===");
-        System.out.println("1 – Combo Master");
-        System.out.println("2 – Super Combo");
+        System.out.println("1 - Cheddar Combo");
+        System.out.println("2 – Chicken Combo");
+        System.out.println("3 - Fish Combo");
         System.out.print("Sua escolha: ");
         int escolha = in.nextInt();
 
-        /*
 
-         ComboFacade facade = new ComboFacade();
+        ComboFacade facade = new ComboFacade();
         facade.criarCombo(escolha);
         facade.exibirItens();
         System.out.printf("Total a pagar: R$ %.2f%n", facade.getPrecoTotal());
 
-        */
 
     }
 }
